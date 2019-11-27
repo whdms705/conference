b9ed7be640f20ad11c20bf72c19c674842c3bc6f
whdms705 <whdms705@nate.com>
2019-11-28 01:30:27 +0900

    ehcache 설정 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.demo.meeting")
 public class DemoApplication {
