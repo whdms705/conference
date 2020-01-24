bda49623ea39f9266056fb39adb662ed4926e207
whdms705 <whdms705@nate.com>
2020-01-25 02:10:47 +0900

    Add spring data jpa

---
+++ src/main/java/com/example/demo/Account.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Account {
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false , unique = true)
+    private String username;
+
+    private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
+
+    private String yes;
+
+    @Transient
+    private String no;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
