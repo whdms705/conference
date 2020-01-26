a1d66cb4a8d7e68018815fedc979843991565fe4
whdms705 <whdms705@nate.com>
2020-01-26 11:45:49 +0900

    spring data jpa 1대다 매핑

--- src/main/java/com/example/demo/Account.java
+++ src/main/java/com/example/demo/Account.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -13,19 +14,22 @@ public class Account {
 
     private String password;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date created = new Date();
+    // OneToMany 에 주인의 mappedBy 설정을 해준다.
+    @OneToMany(mappedBy = "owner")
+    private Set<Study> studies = new HashSet<>();
 
-    private String yes;
 
-    @Transient
-    private String no;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name= "street" , column = @Column(name = "home_street"))
-    })
-    private Address address;
+
+
+
+    public Set<Study> getStudies() {
+        return studies;
+    }
+
+    public void setStudies(Set<Study> studies) {
+        this.studies = studies;
+    }
 
     public Long getId() {
         return id;
@@ -49,5 +53,15 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void addStudy(Study study){
+        this.getStudies().add(study);
+        study.setOwner(this);
+    }
+
+    public void removeStudy(Study study){
+        this.getStudies().remove(study);
+        study.setOwner(this);
     }
 }
